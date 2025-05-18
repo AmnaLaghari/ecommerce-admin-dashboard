be194d6c8f0465de45ffa3f76a4e3e34cffebb84
AmnaLaghari <Amna.laghari243@gmail.com>
2025-05-18 16:38:51 +0500

    Add not found 404 page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,13 @@ import Dashboard from '../views/Dashboard.vue'
 import Inventory from '../views/Inventory.vue'
 import AddProduct from '../views/AddProduct.vue'
 import Revenue from '../views/Revenue.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Revenue },
   { path: '/inventory', name: 'Inventory', component: Inventory },
-  { path: '/add-product', name: 'AddProduct', component: AddProduct }
+  { path: '/add-product', name: 'AddProduct', component: AddProduct },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen text-center px-4">
+      <div>
+          <img src="../assets/404.png" alt="404" height="500px" width="1000px" class="my-5">
+
+      </div>
+      <button class="button-primary">
+        <router-link to="/" class="mt-6 inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+            Go Back Home
+        </router-link>
+    </button>
+    </div>
+  </template>
+  
